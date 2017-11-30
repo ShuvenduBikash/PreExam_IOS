90a70e561e3d45a71c6a81c922742d74cef2c134
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-30 21:44:36 +0600

    proce viewCotroller created
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ PreExam/Controller/MeMC.swift
@@ -0,0 +1,34 @@
+//
+//  MeMC.swift
+//  PreExam
+//
+//  Created by Shuvendu Bikash on 30/11/17.
+//  Copyright © 2017 Shuvendu Bikash. All rights reserved.
+//
+
+import UIKit
+
+class MeMC: UIViewController {
+    
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+
+}
