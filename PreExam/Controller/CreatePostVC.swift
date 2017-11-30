834dd1ecbe6965df54d77c9d8fcb5603d8b9a0b2
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-30 23:04:43 +0600

    redirected to create post page
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ PreExam/Controller/CreatePostVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreatePostVC.swift
+//  PreExam
+//
+//  Created by Shuvendu Bikash on 30/11/17.
+//  Copyright © 2017 Shuvendu Bikash. All rights reserved.
+//
+
+import UIKit
+
+class CreatePostVC: UIViewController {
+
+    @IBOutlet weak var sendBtn: UIButton!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+    }
+    
+
+    @IBAction func sendBtnPressed(_ sender: Any) {
+    }
+}
