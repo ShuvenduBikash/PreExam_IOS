8a79bcf336e2257d551741ee555a88d2355a84d4
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-30 14:54:42 +0600

    initial commit
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ PreExam/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PreExam
+//
+//  Created by Shuvendu Bikash on 30/11/17.
+//  Copyright © 2017 Shuvendu Bikash. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PreExam/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PreExam
+//
+//  Created by Shuvendu Bikash on 30/11/17.
+//  Copyright © 2017 Shuvendu Bikash. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
